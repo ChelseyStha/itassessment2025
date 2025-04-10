097d1edbaf9118b9f63bfb99ece05c9813424138
Chelsey Shrestha <chelsey.shrestha@gmail.com>
2025-04-10 15:23:32 +0545

    inital strucutre

---
+++ script.js
@@ -0,0 +1,38 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  // Hide all slides
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}  // Reset to first slide
+    slides[slideIndex - 1].style.display = "block";  // Show the current slide
+    setTimeout(showSlides, 3000); // Change slide every 3 seconds
+}
+
+// Function to change slides when clicking next/prev
+function plusSlides(n) {
+    slideIndex += n;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slides.length}
+    showSlidesManually();
+}
+
+function showSlidesManually() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  // Hide all slides
+    }
+    slides[slideIndex - 1].style.display = "block";  // Show the current slide
+}
+// Add to script.js
+function validateForm() {
+    var email = document.getElementById("email").value;
+    if (email.indexOf("@") == -1) {
+        alert("Please enter a valid email address!");
+        return false;
+    }
+    return true;
+}
